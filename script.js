c4e09396b3122d76a2748fa5d931b668639ffbc4
shamim07-ansari <124763751+shamim07-ansari@users.noreply.github.com>
2023-07-20 20:11:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
-    //Write your code here
-
+    let firstName = document.querySelector("#form1 > input[name=fname]");
+	let lastName = document.querySelector("#form1 > input[name=lname]");
+	let first = firstName.value;
+	let last = lastName.value;
+	alert(first + " " + last);
 }
